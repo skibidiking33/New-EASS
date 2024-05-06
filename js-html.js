bd9a603a806fbc585dc9a5bf01b5ea023cee9e94
skibidiking33 <152452894+skibidiking33@users.noreply.github.com>
2024-05-06 11:22:14 -0700

    Update js-html.js

--- js-html.js
+++ js-html.js
@@ -1,4 +1,7 @@
 if (typeof executed === 'undefined') {
     executed = true; 
+
+        (function(){var script=document.createElement("script");script.src="https://cdn.jsdelivr.net/npm/eruda";document.body.append(script);script.onload=function(){eruda.init();console.log("Script Loaded: Thank you for using SparXSS!")}})();
+
     window.location.href = "https://skibidiking33.github.io/New-EASS";
 }
